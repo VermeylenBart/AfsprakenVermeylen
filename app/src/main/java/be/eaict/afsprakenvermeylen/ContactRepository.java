ff17bab074182d232cb4045bf363358c9c075f52
Bart Vermeylen <bart.vermeylen1@gmail.com>
2017-11-14 15:02:34 +0100

    Revert "nothing special"
    This reverts commit 302c4cc289edf615946fd4e225219f8efe8a36fc.

--- app/src/main/java/be/eaict/afsprakenvermeylen/ContactRepository.java
+++ app/src/main/java/be/eaict/afsprakenvermeylen/ContactRepository.java
@@ -14,7 +14,7 @@ public class ContactRepository implements IContactRepository {
 
 
     public List<Contact> getContacts() {
-        List<Contact> contacts = new ArrayList<>();
+        List<Contact> contacts = new ArrayList<Contact>();
 
         contacts.add(new Contact("Dr. Van Limburgen", R.drawable.contact1));
         contacts.add(new Contact("Dr. Geneens", R.drawable.contact2));
